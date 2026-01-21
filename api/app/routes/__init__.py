f8d28779a11363f3555fd5e982b53346cd113c84
gpelai <gabrielpelaicontato@gmail.com>
2026-01-21 20:08:55 -0300

    add: investments routes

--- api/app/routes/__init__.py
+++ api/app/routes/__init__.py
@@ -1,7 +1,9 @@
 from .bills import bp as bills_bp
+from .investments import bp as investments_bp
 from .health import bp as health_bp
 
 
 def register_blueprint(app):
+    app.register_blueprint(investments_bp)
     app.register_blueprint(health_bp)
     app.register_blueprint(bills_bp)
